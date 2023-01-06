e19e90adbce84972599f3519fdfb286fb871b015
= <astakhov00@gmail.com>
2023-01-06 18:53:03 -0500

    initial commit

--- 4.js
+++ 4.js
@@ -1,4 +1,6 @@
-/* Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+/* 4. Median of Two Sorted Arrays
+
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 
 The overall run time complexity should be O(log (m+n)).
 
